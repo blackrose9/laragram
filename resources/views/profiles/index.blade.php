a4be17f329174ff44286a9f243d33ca6d2d7e7c2
blackrose9 <sarah.mwendwa@outlook.com>
2020-10-02 00:19:42 +0200

    Display every post added by a specific user ordered by time they were posted.

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -9,10 +9,10 @@
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-baseline">
                 <h1>{{$user->username}}</h1>
-                <a href="#">Add New Post</a>
+                <a href="/p/create">Add New Post</a>
             </div>
             <div class="d-flex">
-                <div class="pr-5"><strong>85</strong> posts</div>
+                <div class="pr-5"><strong>{{$user->posts->count()}}</strong> posts</div>
                 <div class="pr-5"><strong>171</strong> followers</div>
                 <div class="pr-5"><strong>135</strong> following</div>
             </div>
@@ -22,9 +22,11 @@
         </div>
     </div>
     <div class="row pt-5">
-        <div class="col-4"> <img src="https://scontent-lga3-1.cdninstagram.com/v/t51.2885-15/e15/10919380_347236018794493_412094876_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&amp;_nc_cat=101&amp;_nc_ohc=25v7juqzpMcAX-nP-eL&amp;_nc_tp=18&amp;oh=3e02b0baa7e5240fb3b22c58b8b3ec43&amp;oe=5F9E85D7" class="w-100" alt=""></div>
-        <div class="col-4"> <img src="https://scontent-lga3-1.cdninstagram.com/v/t51.2885-15/e15/10831943_1375540699408753_2121813620_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&amp;_nc_cat=101&amp;_nc_ohc=7OIMzcKDFPcAX_E7V8k&amp;_nc_tp=18&amp;oh=42e21d9f12682b5653f39b29c21a7c5d&amp;oe=5F9BF472" class="w-100" alt=""></div>
-        <div class="col-4"> <img src="https://scontent-lga3-1.cdninstagram.com/v/t51.2885-15/e15/10809976_531424123661356_341007288_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&amp;_nc_cat=104&amp;_nc_ohc=YxUL5z4kmPwAX-qb6Ax&amp;_nc_tp=18&amp;oh=c9109e0604ab1e2da68e510190bdfb5e&amp;oe=5F9FA8D1" class="w-100" alt=""></div>
+        @foreach($user->posts as $post)
+            <div class="col-4 pb-4">
+                <a href="/p/{{$post->id}}"><img src="/storage/{{$post->image}}" class="w-100" alt=""></a>
+            </div>
+        @endforeach
     </div>
 </div>
 @endsection
